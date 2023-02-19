8e0ba269f1c0a79d19c8b15090321257753f5a87
Luigi Blasquez <luigiblasquez@gmail.com>
2023-02-18 16:39:45 -0800

    limelight port updated and documented

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -7,6 +7,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+//NOTICE Limelight is connected to port 22 on the power distribution
+
 /*
  * Singleton class to handle the Limelight subsystem base
  * <ul>
