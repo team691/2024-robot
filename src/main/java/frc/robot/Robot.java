0c9fdfd548410b834dad7a79e73caada8fcf3f3f
Luigi Blasquez <luigiblasquez@gmail.com>
2023-03-11 14:48:40 -0800

    ocd edit, revert it later or whatevs

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+//import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 // camera
 import edu.wpi.first.cameraserver.CameraServer;
