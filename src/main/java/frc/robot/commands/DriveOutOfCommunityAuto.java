8d903031118db8b32e4660a523dc24fd896fe5e6
Luigi Blasquez <luigiblasquez@gmail.com>
2023-03-18 16:19:24 -0700

    change back to drive timeout

--- src/main/java/frc/robot/commands/DriveOutOfCommunityAuto.java
+++ src/main/java/frc/robot/commands/DriveOutOfCommunityAuto.java
@@ -18,7 +18,7 @@ public class DriveOutOfCommunityAuto extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new DriveDistanceCommand(14, AutoConstants.kAutoDriveSpeed,0, drive ),
+      new DriveTimeCommand(AutoConstants.kAutoDriveSpeed,0, drive, 4.4 ),
       new LimelightTrackingCommand(lime, drive)
     );
   }
