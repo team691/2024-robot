382a3643186d257dbf60381d3039b3d273659a4f
spacecastor <simonpolluxcastor@gmail.com>
2023-03-17 18:49:21 -0700

    test commands base?

---
+++ src/main/java/frc/robot/commands/testCommands/Test.java
@@ -0,0 +1,45 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.testCommands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
+
+//import frc.robot.subsystems.ExampleSubsystem;
+//import edu.wpi.first.wpilibj2.command.CommandBase;
+//import edu.wpi.first.wpilibj2.command.Commands;
+
+// Constants
+//import frc.robot.Constants.AutoConstants;
+
+// Subsystems
+//import frc.robot.subsystems.DriveTrain;
+//import frc.robot.subsystems.New.Arm;
+
+
+/*// Shuffleboard
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard*/
+
+public final class Test {
+  // private final DriveTrain m_drive = new DriveTrain();
+  /** Example static factory for an autonomous command. */
+
+ /*
+  public static CommandBase printRearLeftEncoder(DriveTrain m_drive){
+     m_drive
+    .printRearLeftEncoderDistance();
+  }
+ 
+  public static CommandBase retractArm(Arm m_arm){
+    return m_arm
+    .extendArm(AutoConstants.retractionTime);
+  }
+  */
+  private Test() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
--- src/main/java/frc/robot/commands/testCommands/encodersTestCommand.java
+++ src/main/java/frc/robot/commands/testCommands/encodersTestCommand.java
@@ -2,31 +2,25 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-// REFERENCE 
-package frc.robot.commands;
+package frc.robot.commands.testCommands;
 
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveTrain;
 
-/** An example command that uses an example subsystem. */
-public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final ExampleSubsystem m_subsystem;
+public class encodersTestCommand extends CommandBase {
+  private final DriveTrain m_drive;
 
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public ExampleCommand(ExampleSubsystem subsystem) {
-    m_subsystem = subsystem;
+  /** Creates a new encodersTestCommand. */
+  public encodersTestCommand(DriveTrain drive) {
+    m_drive = drive;
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    m_drive.resetEncoders();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
