e32ef393a6e96287894fb47182fc4da15fb0ae7c
Luigi Blasquez <luigiblasquez@gmail.com>
2023-03-25 14:12:55 -0700

    speed changes

--- src/main/java/frc/robot/subsystems/New/Claw.java
+++ src/main/java/frc/robot/subsystems/New/Claw.java
@@ -33,10 +33,10 @@ public class Claw extends SubsystemBase {
 
   public void telopClawControls(double up, double down) {
     if (up > down){
-      wristMotor.set(up/6);
+      wristMotor.set(up);
     }
     else if (down > up){
-      wristMotor.set(-down/6);
+      wristMotor.set(-down);
     }
     else{
       wristMotor.stopMotor();
