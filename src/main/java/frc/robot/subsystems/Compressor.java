b5b8b9cab628844c249aa0de2086d71ba78270a2
Luigi Blasquez <luigiblasquez@gmail.com>
2023-03-29 19:09:06 -0700

    please???

--- src/main/java/frc/robot/subsystems/Compressor.java
+++ src/main/java/frc/robot/subsystems/Compressor.java
@@ -3,7 +3,8 @@ package frc.robot.subsystems;
 //imports n stuff
 import edu.wpi.first.wpilibj.compressor;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Compressor {
+public class Compressor extends SubsystemBase{
     
 }
