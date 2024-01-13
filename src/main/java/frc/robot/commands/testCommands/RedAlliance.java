5cf8de39a5f54fcf98f6784020550c7f3ca40f9c
Gautham <korrapatigautham@gmail.com>
2024-01-13 12:28:35 -0800

    redalliance april tag identification and doing stufff with id

--- src/main/java/frc/robot/commands/testCommands/RedAlliance.java
+++ src/main/java/frc/robot/commands/testCommands/RedAlliance.java
@@ -1,8 +1,33 @@
 package frc.robot.commands.testCommands;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.apriltag.*;
+import java.awt.image.BufferedImage;
 
+// Red Alliance: 1, 2, 3, 4, 5
+
+// Blue Alliance: 6, 7, 8, 9, 10
 
 public class RedAlliance {
-    // NetWorkTableInstance.getDefault().getTable("limelight").getEntry("<variablename>") --> fix later
+    private AprilTagDetector detector;
+
+    public RedAlliance() {
+        // Initialize the AprilTagDetector with the appropriate settings
+        detector = new AprilTagDetector();
+    }
+
+    public void processImage(BufferedImage image) {
+        // Detect AprilTags in the provided image
+        TagDetectionArray detections = detector.processImage(image);
+
+        // Process the detected tags
+        for (int i = 1; i < 5; i++) {
+            TagDetectionArray detection = detections.get(i);
+            // Access information about the detected tag (e.g., ID, pose)
+            int tagId = TagDetectionArray.id;
+            double[] translation = detection.cxy;
+
+            // Do something with the detected tag information
+            // (e.g., send data to other subsystems, update robot state)
+        }
+    }
 }
+
---
+++ src/main/java/frc/robot/commands/testCommands/TagDetectionArray.java
@@ -0,0 +1,11 @@
+package frc.robot.commands.testCommands;
+
+public class TagDetectionArray {
+
+    public static final int id = 1;
+    public double[] cxy;
+    public TagDetectionArray get(int i) {
+        return null;
+    }
+
+}
